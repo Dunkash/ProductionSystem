7e1e8293da96d62a9929599bfb150e574ed8ee78
nsilantev91 <45269218+nsilantev91@users.noreply.github.com>
2021-11-25 23:15:04 +0300

    fixes

--- ProductionSystem/Form1.Designer.cs
+++ ProductionSystem/Form1.Designer.cs
@@ -36,6 +36,8 @@ namespace ProductionSystem
             this.backword_btn = new System.Windows.Forms.Button();
             this.clear_btn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.d_facts_box = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // facts_box
@@ -60,7 +62,7 @@ namespace ProductionSystem
             // text_box
             // 
             this.text_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.text_box.Location = new System.Drawing.Point(493, 41);
+            this.text_box.Location = new System.Drawing.Point(764, 41);
             this.text_box.Name = "text_box";
             this.text_box.Size = new System.Drawing.Size(851, 455);
             this.text_box.TabIndex = 2;
@@ -80,18 +82,19 @@ namespace ProductionSystem
             // backword_btn
             // 
             this.backword_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.backword_btn.Location = new System.Drawing.Point(212, 495);
+            this.backword_btn.Location = new System.Drawing.Point(386, 495);
             this.backword_btn.Name = "backword_btn";
             this.backword_btn.Size = new System.Drawing.Size(168, 83);
             this.backword_btn.TabIndex = 4;
             this.backword_btn.Text = "Обратный вывод";
             this.backword_btn.UseVisualStyleBackColor = true;
+            this.backword_btn.Visible = false;
             this.backword_btn.Click += new System.EventHandler(this.backword_btn_Click);
             // 
             // clear_btn
             // 
             this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.clear_btn.Location = new System.Drawing.Point(493, 495);
+            this.clear_btn.Location = new System.Drawing.Point(764, 495);
             this.clear_btn.Name = "clear_btn";
             this.clear_btn.Size = new System.Drawing.Size(168, 83);
             this.clear_btn.TabIndex = 5;
@@ -103,17 +106,40 @@ namespace ProductionSystem
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(488, 13);
+            this.label2.Location = new System.Drawing.Point(759, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "Логи работы";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(381, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Выведенные факты";
+            this.label3.Visible = false;
+            // 
+            // d_facts_box
+            // 
+            this.d_facts_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d_facts_box.FormattingEnabled = true;
+            this.d_facts_box.Location = new System.Drawing.Point(386, 41);
+            this.d_facts_box.Name = "d_facts_box";
+            this.d_facts_box.Size = new System.Drawing.Size(362, 429);
+            this.d_facts_box.TabIndex = 7;
+            this.d_facts_box.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1363, 647);
+            this.ClientSize = new System.Drawing.Size(1642, 680);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.d_facts_box);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.clear_btn);
             this.Controls.Add(this.backword_btn);
@@ -137,6 +163,8 @@ namespace ProductionSystem
         private System.Windows.Forms.Button backword_btn;
         private System.Windows.Forms.Button clear_btn;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox d_facts_box;
     }
 }
 
--- ProductionSystem/Form1.cs
+++ ProductionSystem/Form1.cs
@@ -17,18 +17,22 @@ namespace ProductionSystem
         public Form1()
         {
             InitializeComponent();
-            var factsAndRules = Functions.Parse("../../database.txt");
-            facts = factsAndRules.Item1;
-            rules = factsAndRules.Item2;
+           
+            ReLoadData();
             fillFacts();
             text_box.Height = facts_box.Height;
         }
-
+        private void ReLoadData()
+        {
+            var factsAndRules = Functions.Parse("../../database.txt");
+            facts = factsAndRules.Item1;
+            rules = factsAndRules.Item2;
+        }
         private void fillFacts()
         {
-            foreach (var facts in facts)
+            foreach (var fact in facts)
             {
-                facts_box.Items.Add(facts);
+                facts_box.Items.Add(fact);
             }
         }
 
@@ -44,7 +48,11 @@ namespace ProductionSystem
         }
         private void forward_btn_Click(object sender, EventArgs e)
         {
+            ReLoadData();
+            text_box.Text = "";
             var selectedFacts = facts_box.CheckedItems.Cast<string>().ToHashSet();
+            var dFacts = d_facts_box.CheckedItems.Cast<string>().ToHashSet();
+            selectedFacts = selectedFacts.Union(dFacts).ToHashSet();
             var count = selectedFacts.Count + 1;
             int step = 1;
             text_box.Text += "Прямой поиск\n";
@@ -76,14 +84,23 @@ namespace ProductionSystem
                 }
                 step++;
             }
+            var sel = facts_box.CheckedItems.Cast<string>().ToHashSet();
+            var res = Difference(selectedFacts.ToList(), sel.ToList());
+            foreach (var fact in res)
+            {
+                if(!d_facts_box.Items.Contains(fact))
+                    d_facts_box.Items.Add(fact);
+            }
+            label3.Visible = true;
+            d_facts_box.Visible = true;
+            backword_btn.Visible = true;
         }
 
         private void backword_btn_Click(object sender, EventArgs e)
         {
-            /*var selectedFacts = facts_box.CheckedItems.Cast<string>().ToHashSet<string>();
-            var res1 = Functions.Backward(selectedFacts, rules);
-            var res = selectedFacts;
-            var count = res.Count + 1;
+            ReLoadData();
+            text_box.Text = "";
+            var selectedFacts = d_facts_box.CheckedItems.Cast<string>().ToHashSet();
             int step = 1;
             text_box.Text += "Обратный поиск\n";
             text_box.Text += "Выбранные факты:\n";
@@ -93,23 +110,40 @@ namespace ProductionSystem
                 text_box.Text += $"{factNum}:  {fact}\n";
                 factNum++;
             }
-            text_box.Text += '\n';*/
-           /*foreach (var rule in rules)
+            text_box.Text += '\n';
+            bool check = false;
+            while (true)
             {
-                if (!rule.Evaluated && res.Contains(rule.Action))
+                foreach (var rule in rules)
                 {
-                    foreach (var condition in rule.Conditions)
-                        res.Add(condition);
-                    rule.Evaluated = true;
+                    if (!rule.Evaluated && selectedFacts.Contains(rule.Action))
+                    {
+                        text_box.Text += $"\nШаг # {step}\n";
+                        text_box.Text += "--------------------------------------------------------------\n";
+                        text_box.Text += $"Полученное правило: {rule}\n";
+                        text_box.Text += "--------------------------------------------------------------\n";
+
+                        rule.Evaluated = true;
+                        check = true;
+                    }
                 }
-            }*/
+                step++;
+                if (!check)
+                    break;
+                check = false;
+            }
         }
 
         private void clear_btn_Click(object sender, EventArgs e)
         {
+            ReLoadData();
             facts_box.Items.Clear();
             fillFacts();
             text_box.Text = "";
+            d_facts_box.Text = "";
+            label3.Visible = false;
+            d_facts_box.Visible = false;
+            backword_btn.Visible = false;
         }
     }
 }
